ed0bf0ed4f56b8791db10c9138fa0bc00b8942b2
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-27 14:52:00 -0800

    implement TimelineEvent component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
+  const event = {
+    person: 'Adele Goldberg',
+    status: 'In Smalltalk, everything happens somewhere else.',
+    timeStamp: '2018-05-18T22:12:03Z'
+  }
 
   // Customize the code below
   return (
@@ -14,6 +20,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent event={event} />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <div className='timeline-event'>
+      <p className='event-person'>{ props.event.person }</p>
+      <p className='event-time'><Timestamp time={ props.event.timeStamp } /></p>
+      <p className='event-status'>{ props.event.status }</p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
